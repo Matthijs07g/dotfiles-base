94e769e0394eccf37b00dab46aebae194b2ffdcc
matthijs07g <matthijs07g@gmail.com>
2026-03-31 11:08:52 +0200

    Added warning in the install script

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-
 set -e
+
+# Warn if running inside Hyprland
+if [ "$XDG_CURRENT_DESKTOP" = "Hyprland" ]; then
+    echo "WARNING: You are running this inside Hyprland."
+    echo "It is recommended to run this from a TTY (Ctrl+Alt+F3) or another DE."
+    read -p "Continue anyway? (y/N) " confirm
+    if [ "$confirm" != "y" ]; then
+        echo "Aborted."
+        exit 1
+    fi
+fi
 
 echo "Installing packages..."
 sudo pacman -S --needed \
@@ -10,8 +20,8 @@ sudo pacman -S --needed \
 echo "Stowing dotfiles..."
 PACKAGES=(hypr alacritty)
 for package in "${PACKAGES[@]}"; do
-	rm -rf ~/.config/$package
-    	stow --target="$HOME" "$package"
+    rm -rf ~/.config/$package
+    stow --target="$HOME" "$package"
 done
 
 echo "Done!"
